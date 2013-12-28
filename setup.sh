74dfd8ee140e073e13c62232c4c61ba564312712
izumineid <hirohumi1989@gmail.com>
2013-12-28 13:57:29 +0900

    tmux-powerlineのテーマをdotfiles下に

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ mkdir -p $HOME/dotfiles/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim $HOME/dotfiles/.vim/bundle/neobundle.vim
 git clone https://github.com/Shougo/vimproc $HOME/dotfiles/.vim/bundle/vimproc
 
-for file in `find $HOME/dotfiles -name '.*' | grep -v 'dotfiles/.git$' | grep -v 'dotfiles/.gitignore$' | perl -nle 'm!dotfiles/(.+)$! and print $1'`; do
+for file in `find $HOME/dotfiles -name '.*' | grep -v 'dotfiles/.git$' | grep -v 'dotfiles/.gitignore$' | grep -v 'dotfiles/.tmux$' | perl -nle 'm!dotfiles/(.+)$! and print $1'`; do
   ln -s $HOME/dotfiles/$file $HOME/$file
 done
